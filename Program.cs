27bc72cc1991d6dcac2d06595fe34bd312f9c77a
martin010919 <70171040+martin010919@users.noreply.github.com>
2021-04-09 12:48:24 +0200

    .cs C# file
    Have fun

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace másofokú_éles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            masodfoko();
+            Console.ReadKey();
+
+        }
+        static void masodfoko()
+        {
+            double a;
+            double b;
+            double c;
+            double d;
+            double x1;
+            double x2;
+
+            Console.WriteLine("Quadratic equation");
+            Console.WriteLine("Input 'a' ");
+            a = Convert.ToDouble(Console.ReadLine());
+            if (a == 0)
+            {
+                Console.WriteLine(" Not Do-able");
+            }
+            else
+            {
+                Console.WriteLine("Input 'b' ");
+                b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Input 'c' értékét");
+                c = Convert.ToDouble(Console.ReadLine());
+
+                d = b * b - 4 * a * c;
+
+                if (d < 0)
+                {
+                    Console.WriteLine("Not Do-able");
+                }
+                else
+                {
+                    x1 = -b + 4 * a * c;
+
+                    x2 = -b + 4 * a * c;
+
+
+
+                }
+
+            }
+        }
+    }
+}
